26d0b3066bf54efb89c5a247ca0d501103846aed
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-01-26 14:25:45 -0400

    Creation of initial main view for iShaer.

--- ishaer-app/src/App.js
+++ ishaer-app/src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import { osName } from 'react-device-detect';
 import './App.scss';
 
 function App() {
+  let renderContent = () => {
+    if (osName === 'Windows') {
+      return (
+        <ol type="1">
+          <li>Copy (Ctrl + PrtScn) </li>
+          <li> Paste (Ctrl + V) or Drag here </li>
+        </ol>
+      )
+    }
+  
+    return (
+        <ol type="1">
+          <li> Copy (Shift + Ctrl + Cmd + 3) </li>
+          <li> Paste (Shift + V) or Drag here </li>
+        </ol>
+      )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          iShaer
+        </Navbar.Brand>
+      </Navbar>
+      <div className="App">
+        <div className="DropArea">
+          <div className="Status">
+              { renderContent() }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
