21e4979ba0e084ecd45f283b4f7f05ed16013a28
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-02-03 12:13:05 -0400

    Adding new component to show the assets.

---
+++ ishaer-app/src/AssetView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AssetView.scss';
+
+function AssetView() {
+    return (
+        <div className="AssetView">
+            <p>This is the AssetView page.</p>
+        </div>
+    )
+}
+
+export default AssetView;
