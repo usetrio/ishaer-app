1cb629939a109db2b03f2e52782e92645d264b51
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-02-03 13:37:42 -0400

    Importing new component and adding router.

--- ishaer-app/src/index.js
+++ ishaer-app/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import AssetView from './AssetView';
+import { Navbar } from 'react-bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <div className="MainContainer">
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>
+            iShaer
+            </Navbar.Brand>
+        </Navbar>
+        <Router>
+            <Route exact path="/" component={ App } />
+            <Route exact path="/:code" component={ AssetView } />
+        </Router>
+    </div>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
