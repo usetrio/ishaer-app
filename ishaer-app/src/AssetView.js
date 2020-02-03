cdeecb4b7d1a00c2fdd79fe4ea22bbeeb06935e6
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-02-03 14:55:50 -0400

    Adding logic to new component with styles.

--- ishaer-app/src/AssetView.js
+++ ishaer-app/src/AssetView.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from "react-router";
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { appSettings } from './configs/config';
+import copy from 'clipboard-copy';
+
 import './AssetView.scss';
 
-function AssetView() {
+const fetchAsset = async (code) => {
+    const response = await fetch(`${appSettings.apiUrl}/${code}`, {
+        method: 'GET'
+    })
+    const data = await response.json();
+    
+    return data;
+}
+
+const AssetView = () => {
+    let { code } = useParams();
+    let data = fetchAsset(code);
+    const [preview, setPreview] = useState(null);
+    const [url, setUrl] = useState('');
+
+
+    data.then(info => {
+        console.log(info);
+        setPreview(info.asset.url);
+        setUrl(info.asset.public_link);
+    }).catch(err => {
+        console.log(err);
+    });
+
     return (
         <div className="AssetView">
-            <p>This is the AssetView page.</p>
+            <div className="App">
+                <div className="DropArea">
+                    <div className="ImageProgress">
+                        <div className="ImageProgressImage" style={{ backgroundImage: `url(${preview})` }}></div>
+                    </div>
+                    <InputGroup className="mb-3 PublicUrl Visible">
+                        <FormControl
+                        disabled="True"
+                        value={ url }
+                        />
+                        <InputGroup.Append>
+                            <Button variant="light" onClick={ () => { copy( url ); }}> Copy </Button>
+                        </InputGroup.Append>
+                        <InputGroup.Append>
+                            <Button variant="light"> Download </Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </div>
+            </div>
         </div>
     )
 }
